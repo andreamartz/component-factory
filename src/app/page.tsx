2ca764406776579dab3824a0f81cb6d68b1023c1
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2026-06-08 18:01:05 -0500

    Add updates to styles, layout and main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,28 @@
 import Image from "next/image";
+import Navbar from "./components/Navbar/Navbar";
+import Blog from "./components/Blog/Blog";
+import UserProfile from "./components/UserProfile/UserProfile";
+import Inputs from "./components/Inputs/Inputs";
+import Buttons from "./components/Buttons/Buttons";
+import Menu from "./components/Menu/Menu";
+import BusinessCard from "./components/BusinessCard/BusinessCard";
+import TodoList from "./components/TodoList/TodoList";
+import { MoodBoard } from "./components/MoodBoard/MoodBoard";
+import QRCodeCard from "./components/QRCodeCard/QRCodeCard";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <>
+      {/* <Navbar /> */}
+      {/* <Blog /> */}
+      {/* <UserProfile /> */}
+      {/* <Inputs /> */}
+      {/* <Buttons /> */}
+      {/* <Menu /> */}
+      {/* <BusinessCard /> */}
+      {/* <TodoList /> */}
+      {/* <MoodBoard /> */}
+      <QRCodeCard image="./QRCodeCard-image-qr-code.png" title="Improve your front-end skills by building projects" description="Scan the QR code to visit Frontend Mentor and take your coding skills to the next level"/>
+    </>
   );
 }
